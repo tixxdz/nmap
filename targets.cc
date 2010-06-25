tixxdz/nmap

commit e07387e376bc695d992d81b6d703397a433fbe44

Break apart target_needs_new_hostgroup to it's easier to read, not just one big expression. Also fix a dumb bug I just introduced where I was comparing the same value with itself.
git-svn-id: https://svn.nmap.org/nmap@17892 e0a8ed71-7df4-0310-8962-fdc924857419

targets.cc
@@ -412,11 +412,30 @@ static void massping(Target *hostbatch[], int num_hosts, struct scan_lists *port
      3. it is directly connected when the other hosts are not, or vice versa.
    These restrictions only apply for raw scans. */
 static bool target_needs_new_hostgroup(const HostGroupState *hs, const Target *target) {
-  return o.af() == AF_INET && o.isr00t && hs->current_batch_sz > 0 && 
-    target->deviceName() && 
-    (target->v4source().s_addr != target->v4source().s_addr || 
-     strcmp(hs->hostbatch[0]->deviceName(), target->deviceName()) != 0 ||
-     target->directlyConnected() != target->directlyConnected());
+  /* We've just started a new hostgroup, so any target is acceptable. */
+  if (hs->current_batch_sz == 0)
+    return false;
+
+  /* There are no restrictions on non-root scans. */
+  if (!(o.af() == AF_INET && o.isr00t))
+    return false;
+
+  /* Different interface name? */
+  if (hs->hostbatch[0]->deviceName() != NULL &&
+      target->deviceName() != NULL &&
+      strcmp(hs->hostbatch[0]->deviceName(), target->deviceName()) != 0) {
+    return true;
+  }
+
+  /* Different source address? */
+  if (hs->hostbatch[0]->v4source().s_addr != target->v4source().s_addr)
+    return true;
+
+  /* Different direct connectedness? */
+  if (hs->hostbatch[0]->directlyConnected() != target->directlyConnected())
+    return true;
+
+  return false;
 }
 
 Target *nexthost(HostGroupState *hs, TargetGroup *exclude_group,

commit 49cf0e909ef96b596ee0dfd7be64deb35f9c82a5

Check for target->deviceName == NULL in target_needs_new_hostgroup in targets.cc, and return false if it is so. This indicates that we are not doing a raw scan. target->deviceName != NULL is necessary before checking target->directlyConnected(), otherwise you get this assertion failure when doing any non-root scan (such as -sT or -sL) as root with two or more targets:
nmap: Target.cc:369: bool Target::directlyConnected() const: Assertion `directly_connected == 0 || directly_connected == 1' failed.
Aborted

This is how the logic originally worked, and I accidentally broke it in
r17892.


git-svn-id: https://svn.nmap.org/nmap@18407 e0a8ed71-7df4-0310-8962-fdc924857419

targets.cc
@@ -382,12 +382,11 @@ static bool target_needs_new_hostgroup(const HostGroupState *hs, const Target *t
     return false;
 
   /* There are no restrictions on non-root scans. */
-  if (!(o.af() == AF_INET && o.isr00t))
+  if (!(o.af() == AF_INET && o.isr00t && target->deviceName() != NULL))
     return false;
 
   /* Different interface name? */
   if (hs->hostbatch[0]->deviceName() != NULL &&
-      target->deviceName() != NULL &&
       strcmp(hs->hostbatch[0]->deviceName(), target->deviceName()) != 0) {
     return true;
   }