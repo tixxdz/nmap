tixxdz/nmap

commit f1d99a11ef9da5e12956ef6603e10802badf2ffd

Merge from /nmap-exp/patrick/nse-nsock-maintenance.
This is a maintenance fix for the NSE Nsock library binding. The patch focuses
on code correctness and simplicity. The patch also brings some initial updates
with an eye towards the upcoming Lua 5.2 release. See [1] for a post concerning
this branch.

[1] http://seclists.org/nmap-dev/2010/q3/710



git-svn-id: https://svn.nmap.org/nmap@20267 e0a8ed71-7df4-0310-8962-fdc924857419

nse_dnet.cc
@@ -1,0 +1,313 @@
+#include "nsock.h"
+#include "nmap_error.h"
+#include "NmapOps.h"
+#include "utils.h"
+#include "tcpip.h"
+#include "protocols.h"
+#include "libnetutil/netutil.h"
+
+#include "nse_main.h"
+#include "nse_utility.h"
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+#include <assert.h>
+
+extern NmapOps o;
+
+/* Map of dnet userdata to ethernet device userdata */
+#define ETH_CACHE_DNET_ETH  0
+/* Map of ethernet device string identifier to ethernet device userdata */
+#define ETH_CACHE_DEVICE_ETH 1
+
+/* metatable entries in the registry */
+#define DNET_METATABLE  "DNET_METATABLE"
+#define DNET_ETH_METATABLE  "DNET_ETH_METATABLE"
+
+typedef struct nse_dnet_udata
+{
+  eth_t *eth;
+  int sock; /* raw ip socket */
+  int interface; /* integer reference in registry for interface string */
+} nse_dnet_udata;
+
+LUALIB_API int l_dnet_new (lua_State *L)
+{
+  nse_dnet_udata *udata;
+
+  udata = (nse_dnet_udata *) lua_newuserdata(L, sizeof(nse_dnet_udata));
+  luaL_getmetatable(L, DNET_METATABLE);
+  lua_setmetatable(L, -2);
+  udata->eth = NULL;
+  udata->sock = -1;
+  udata->interface = LUA_NOREF;
+
+  return 1;
+}
+
+LUALIB_API int l_dnet_get_interface_link (lua_State *L)
+{
+  struct interface_info *ii = getInterfaceByName(luaL_checkstring(L, 1));
+
+  if (ii == NULL)
+    return luaL_argerror(L, 1, "bad interface");
+
+  switch (ii->device_type)
+  {
+    case devt_ethernet:
+      lua_pushliteral(L, "ethernet");
+      return 1;
+    case devt_loopback:
+      lua_pushliteral(L, "loopback");
+      return 1;
+    case devt_p2p:
+      lua_pushliteral(L, "p2p");
+      return 1;
+    case devt_other:
+    default:
+      lua_pushnil(L);
+      return 1;
+  }
+}
+
+static int close_eth (lua_State *L)
+{
+  eth_t **eth = (eth_t **) luaL_checkudata(L, 1, DNET_ETH_METATABLE);
+  assert(*eth != NULL);
+  eth_close(*eth);
+  *eth = NULL;
+  return success(L);
+}
+
+static eth_t *open_eth_cached (lua_State *L, int dnet_index, const char *device)
+{
+  eth_t **eth;
+
+  lua_rawgeti(L, LUA_ENVIRONINDEX, ETH_CACHE_DNET_ETH);
+  lua_rawgeti(L, LUA_ENVIRONINDEX, ETH_CACHE_DEVICE_ETH);
+  lua_getfield(L, -1, device);
+  if (!lua_isuserdata(L, -1))
+  {
+    eth = (eth_t **) lua_newuserdata(L, sizeof(eth_t *));
+    *eth = eth_open(device);
+    if (*eth == NULL)
+      luaL_error(L, "unable to open dnet on ethernet interface %s", device);
+    luaL_getmetatable(L, DNET_ETH_METATABLE);
+    lua_setmetatable(L, -2);
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -4, device);
+    lua_replace(L, -2); /* replace nil */
+  }
+  eth = (eth_t **) lua_touserdata(L, -1);
+
+  lua_pushvalue(L, dnet_index);
+  lua_pushvalue(L, -2); /* eth_t userdata */
+  lua_rawset(L, -5); /* add to ETH_CACHE_DNET_ETH */
+  lua_pop(L, 3); /* ETH_CACHE_DNET_ETH, ETH_CACHE_DEVICE_ETH, eth_t userdata */
+
+  return *eth;
+}
+
+static int ethernet_open (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+  const char *interface_name = luaL_checkstring(L, 2);
+  struct interface_info *ii = getInterfaceByName(interface_name);
+
+  if (ii == NULL || ii->device_type != devt_ethernet)
+    return luaL_argerror(L, 2, "device is not valid ethernet interface");
+
+  udata->eth = open_eth_cached(L, 1, interface_name);
+  lua_pushvalue(L, 2);
+  udata->interface = luaL_ref(L, LUA_REGISTRYINDEX);
+
+  return success(L);
+}
+
+static int ethernet_close (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+
+  udata->eth = NULL;
+
+  lua_rawgeti(L, LUA_ENVIRONINDEX, ETH_CACHE_DNET_ETH);
+  lua_pushvalue(L, 1);
+  lua_pushnil(L);
+  lua_rawset(L, -3);
+
+  luaL_unref(L, LUA_REGISTRYINDEX, udata->interface);
+  udata->interface = LUA_NOREF;
+
+  return success(L);
+}
+
+static int ethernet_send (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+  if (udata->eth == NULL)
+    return luaL_error(L, "dnet ethernet interface is not open");
+  eth_send(udata->eth, luaL_checkstring(L, 2), lua_objlen(L, 2));
+  return success(L);
+}
+
+static int ip_open (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+  udata->sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+  if (udata->sock == -1)
+    return luaL_error(L, "failed to open raw socket: %s (errno %d)",
+        socket_strerror(socket_errno()), socket_errno());
+  broadcast_socket(udata->sock);
+#ifndef WIN32
+  sethdrinclude(udata->sock);
+#endif
+  return success(L);
+}
+
+static int ip_close (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+  if (udata->sock == -1)
+    return safe_error(L, "raw socket already closed");
+  close(udata->sock);
+  udata->sock = -1;
+  return success(L);
+}
+
+static int ip_send (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+  const char *packet = luaL_checkstring(L, 2);
+
+  int ret;
+
+  if (udata->sock == -1)
+    return luaL_error(L, "raw socket not open to send");
+
+  if (lua_objlen(L, 2) < sizeof(struct ip))
+    return luaL_error(L, "ip packet too short");
+
+  if (o.sendpref & PACKET_SEND_ETH)
+  {
+    struct route_nfo route;
+    struct sockaddr_storage srcss, dstss, *nexthop;
+    struct sockaddr_in *srcsin = (struct sockaddr_in *) &srcss;
+    struct sockaddr_in *dstsin = (struct sockaddr_in *) &dstss;
+    struct ip *ip = (struct ip *) packet;
+    u8 dstmac[6];
+    eth_nfo eth;
+
+    /* build sockaddr for target from user packet and determine route */
+    memset(&dstss, 0, sizeof(dstss));
+    dstsin->sin_family = AF_INET;
+    dstsin->sin_addr.s_addr = ip->ip_dst.s_addr;
+
+    if (!nmap_route_dst(&dstss, &route))
+      goto usesock;
+
+    if (route.ii.device_type != devt_ethernet)
+      goto usesock;
+
+    /* above we fallback to using the raw socket if we can't find an (ethernet)
+     * route to the host.  From here on out it's ethernet all the way.
+     */
+
+    /* build sockaddr for source from user packet to determine next hop mac */
+    memset(&srcss, 0, sizeof(srcss));
+    srcsin->sin_family = AF_INET;
+    srcsin->sin_addr.s_addr = ip->ip_src.s_addr;
+
+    if (route.direct_connect)
+      nexthop = &dstss;
+    else
+      nexthop = &route.nexthop;
+
+    if (!getNextHopMAC(route.ii.devfullname, route.ii.mac, &srcss, nexthop, dstmac))
+      return luaL_error(L, "failed to determine next hop MAC address");
+
+    /* Use cached ethernet device, and use udata's eth and interface to keep
+     * track of if we're reusing the same device from the previous packet, and
+     * close the cached device if not.
+     */
+    memset(&eth, 0, sizeof(eth));
+    memcpy(eth.srcmac, route.ii.mac, sizeof(eth.srcmac));
+    memcpy(eth.dstmac, dstmac, sizeof(eth.dstmac));
+
+    /* close any current ethernet associated with this userdata */
+    lua_pushcfunction(L, ethernet_close);
+    lua_pushvalue(L, 1);
+    lua_call(L, 1, 0);
+
+    udata->eth = eth.ethsd = open_eth_cached(L, 1, route.ii.devname);
+    lua_pushstring(L, route.ii.devname);
+    udata->interface = luaL_ref(L, LUA_REGISTRYINDEX);
+
+    ret = send_ip_packet(udata->sock, &eth, (u8 *) packet, lua_objlen(L, 2));
+  } else {
+usesock:
+#ifdef WIN32
+    if (strlen(dev) > 0)
+      win32_warn_raw_sockets(dev);
+#endif
+    ret = send_ip_packet(udata->sock, NULL, (u8 *) packet, lua_objlen(L, 2));
+  }
+  if (ret == -1)
+    return luaL_error(L, "error while sending: %s (errno %d)",
+        socket_strerror(socket_errno()), socket_errno());
+
+  return success(L);
+}
+
+static int gc (lua_State *L)
+{
+  nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
+
+  lua_pushcfunction(L, ip_close);
+  lua_pushvalue(L, 1);
+  lua_call(L, 1, 0);
+  lua_pushcfunction(L, ethernet_close);
+  lua_pushvalue(L, 1);
+  lua_call(L, 1, 0);
+
+  luaL_unref(L, LUA_REGISTRYINDEX, udata->interface);
+  return 0;
+}
+
+LUALIB_API int luaopen_dnet (lua_State *L)
+{
+  static const luaL_reg l_dnet[] = {
+    {"ethernet_open", ethernet_open},
+    {"ethernet_close", ethernet_close},
+    {"ethernet_send", ethernet_send},
+    {"ip_open", ip_open},
+    {"ip_close", ip_close},
+    {"ip_send", ip_send},
+    {NULL, NULL}
+  };
+
+  lua_createtable(L, 2, 0);
+  lua_replace(L, LUA_ENVIRONINDEX);
+  weak_table(L, 0, 0, "k"); /* dnet udata weak, eth device strong */
+  lua_rawseti(L, LUA_ENVIRONINDEX, ETH_CACHE_DNET_ETH);
+  weak_table(L, 0, 0, "v"); /* eth_device weak */
+  lua_rawseti(L, LUA_ENVIRONINDEX, ETH_CACHE_DEVICE_ETH);
+
+  luaL_newmetatable(L, DNET_METATABLE);
+  lua_createtable(L, 0, 5);
+  luaL_register(L, NULL, l_dnet);
+  lua_setfield(L, -2, "__index");
+  lua_newtable(L);
+  lua_setfield(L, -2, "__metatable");
+  lua_pushcfunction(L, gc);
+  lua_setfield(L, -2, "__gc");
+
+  luaL_newmetatable(L, DNET_ETH_METATABLE);
+  lua_pushcfunction(L, close_eth);
+  lua_setfield(L, -2, "__gc");
+
+  return 0;
+}

Makefile.in
@@ -82,9 +82,9 @@ UNINSTALLZENMAP=@UNINSTALLZENMAP@
 UNINSTALLNPING=@UNINSTALLNPING@
 
 ifneq (@LIBLUA_LIBS@,)
-NSE_SRC=nse_main.cc nse_nsock.cc nse_fs.cc nse_nmaplib.cc nse_debug.cc nse_pcrelib.cc nse_binlib.cc nse_bit.cc
-NSE_HDRS=nse_main.h nse_nsock.h nse_fs.h nse_nmaplib.h nse_debug.h nse_pcrelib.h nse_binlib.h nse_bit.h
-NSE_OBJS=nse_main.o nse_nsock.o nse_fs.o nse_nmaplib.o nse_debug.o nse_pcrelib.o nse_binlib.o nse_bit.o
+NSE_SRC=nse_main.cc nse_utility.cc nse_nsock.cc nse_dnet.cc nse_fs.cc nse_nmaplib.cc nse_debug.cc nse_pcrelib.cc nse_binlib.cc nse_bit.cc
+NSE_HDRS=nse_main.h nse_utility.h nse_nsock.h nse_dnet.h nse_fs.h nse_nmaplib.h nse_debug.h nse_pcrelib.h nse_binlib.h nse_bit.h
+NSE_OBJS=nse_main.o nse_utility.o nse_nsock.o nse_dnet.o nse_fs.o nse_nmaplib.o nse_debug.o nse_pcrelib.o nse_binlib.o nse_bit.o
 ifneq (@OPENSSL_LIBS@,)
 NSE_SRC+=nse_openssl.cc nse_ssl_cert.cc
 NSE_HDRS+=nse_openssl.h nse_ssl_cert.h

mswin32/nmap.vcproj
@@ -279,11 +279,19 @@
 				>
 			</File>
 			<File
+				RelativePath="..\nse_utility.cc"
+				>
+			</File>
+			<File
 				RelativePath="..\nse_nmaplib.cc"
 				>
 			</File>
 			<File
 				RelativePath="..\nse_nsock.cc"
+				>
+			</File>
+			<File
+				RelativePath="..\nse_dnet.cc"
 				>
 			</File>
 			<File
@@ -480,11 +488,19 @@
 				>
 			</File>
 			<File
+				RelativePath="..\nse_utility.h"
+				>
+			</File>
+			<File
 				RelativePath="..\nse_nmaplib.h"
 				>
 			</File>
 			<File
 				RelativePath="..\nse_nsock.h"
+				>
+			</File>
+			<File
+				RelativePath="..\nse_dnet.h"
 				>
 			</File>
 			<File

nse_dnet.h
@@ -1,0 +1,8 @@
+#ifndef NMAP_LUA_DNET_H
+#define NMAP_LUA_DNET_H
+
+LUALIB_API int l_dnet_new (lua_State *);
+LUALIB_API int l_dnet_get_interface_link (lua_State *);
+LUALIB_API int luaopen_dnet (lua_State *L);
+
+#endif

nse_main.cc
@@ -1,15 +1,3 @@
-
-#include "nse_main.h"
-
-#include "nse_fs.h"
-#include "nse_nsock.h"
-#include "nse_nmaplib.h"
-#include "nse_bit.h"
-#include "nse_binlib.h"
-#include "nse_pcrelib.h"
-#include "nse_openssl.h"
-#include "nse_debug.h"
-
 #include "nmap.h"
 #include "nmap_error.h"
 #include "portlist.h"
@@ -18,6 +6,17 @@
 #include "timing.h"
 #include "Target.h"
 #include "nmap_tty.h"
+
+#include "nse_main.h"
+#include "nse_utility.h"
+#include "nse_fs.h"
+#include "nse_nsock.h"
+#include "nse_nmaplib.h"
+#include "nse_bit.h"
+#include "nse_binlib.h"
+#include "nse_pcrelib.h"
+#include "nse_openssl.h"
+#include "nse_debug.h"
 
 #define NSE_MAIN "NSE_MAIN" /* the main function */
 #define NSE_TRACEBACK "NSE_TRACEBACK"
@@ -130,7 +129,8 @@ static int host_set_output (lua_State *L)
 
 static int port_set_output (lua_State *L)
 {
-  Port port, *p;
+  Port *p;
+  Port port;
   ScriptResult sr;
   Target *target = get_target(L, 1);
   p = get_port(L, target, &port, 2);
@@ -406,6 +406,9 @@ static int run_main (lua_State *L)
   return 0;
 }
 
+/* Lua 5.2 compatibility macro */
+#define lua_yieldk(L,n,ctx,k)  lua_yield(L,n)
+
 /* int nse_yield (lua_State *L)                            [-?, +?, e]
  *
  * This function will yield the running thread back to NSE, even across script
@@ -413,12 +416,12 @@ static int run_main (lua_State *L)
  * correct and only way to call is as a tail call:
  *   return nse_yield(L);
  */
-int nse_yield (lua_State *L)
+int nse_yield (lua_State *L, int ctx, lua_CFunction k)
 {
   lua_getfield(L, LUA_REGISTRYINDEX, NSE_YIELD);
   lua_pushthread(L);
   lua_call(L, 1, 1); /* returns NSE_YIELD_VALUE */
-  return lua_yield(L, 1); /* yield with NSE_YIELD_VALUE */
+  return lua_yieldk(L, 1, ctx, k); /* yield with NSE_YIELD_VALUE */
 }
 
 /* void nse_restore (lua_State *L, int number)             [-, -, e]
@@ -527,7 +530,14 @@ void open_nse (void)
       fatal("%s: failed to open a Lua state!", SCRIPT_ENGINE);
     lua_atpanic(L_NSE, panic);
 
-    if (lua_cpcall(L_NSE, init_main, (void *) &o.chosenScripts) != 0)
+#if 0
+    /* Lua 5.2 */
+    lua_pushcfunction(L_NSE, init_main);
+    lua_pushlightuserdata(L_NSE, &o.chosenScripts);
+    if (lua_pcall(L_NSE, 1, 0, 0))
+#else
+    if (lua_cpcall(L_NSE, init_main, &o.chosenScripts))
+#endif
       fatal("%s: failed to initialize the script engine:\n%s\n", SCRIPT_ENGINE, 
           lua_tostring(L_NSE, -1));
   }
@@ -540,7 +550,14 @@ void script_scan (std::vector<Target *> &targets, stype scantype)
   assert(L_NSE != NULL);
   lua_settop(L_NSE, 0); /* clear the stack */
 
-  if (lua_cpcall(L_NSE, run_main, (void *) &targets) != 0)
+#if 0
+  /* Lua 5.2 */
+  lua_pushcfunction(L_NSE, run_main);
+  lua_pushlightuserdata(L_NSE, &targets);
+  if (lua_pcall(L_NSE, 1, 0, 0))
+#else
+  if (lua_cpcall(L_NSE, run_main, &targets))
+#endif
     error("%s: Script Engine Scan Aborted.\nAn error was thrown by the "
           "engine: %s", SCRIPT_ENGINE, lua_tostring(L_NSE, -1));
 }

nse_main.h
@@ -9,8 +9,8 @@
 
 extern "C" {
   #include "lua.h"
-  #include "lualib.h"
   #include "lauxlib.h"
+  #include "lualib.h"
 }
 
 #include "nmap.h"
@@ -38,7 +38,7 @@ class Target;
 
 
 /* API */
-int nse_yield (lua_State *);
+int nse_yield (lua_State *, int, lua_CFunction);
 void nse_restore (lua_State *, int);
 void nse_destructor (lua_State *, char);
 void nse_base (lua_State *);

nse_nmaplib.cc
@@ -2,6 +2,7 @@
 extern "C" {
   #include "lua.h"
   #include "lauxlib.h"
+  #include "lualib.h"
 }
 
 #include <math.h>
@@ -19,36 +20,14 @@ extern "C" {
 #include "protocols.h"
 
 #include "nse_nmaplib.h"
+#include "nse_utility.h"
 #include "nse_nsock.h"
+#include "nse_dnet.h"
 
 extern NmapOps o;
 
 static const char *NSE_PROTOCOL_OP[] = {"tcp", "udp", "sctp", NULL};
 static const int NSE_PROTOCOL[] = {IPPROTO_TCP, IPPROTO_UDP, IPPROTO_SCTP};
-
-static void setsfield (lua_State *L, int idx, const char *field, const char *what)
-{
-  lua_pushvalue(L, idx);
-  lua_pushstring(L, what); /* what can be NULL */
-  lua_setfield(L, -2, field);
-  lua_pop(L, 1);
-}
-
-static void setnfield (lua_State *L, int idx, const char *field, lua_Number n)
-{
-  lua_pushvalue(L, idx);
-  lua_pushnumber(L, n);
-  lua_setfield(L, -2, field);
-  lua_pop(L, 1);
-}
-
-static void setbfield (lua_State *L, int idx, const char *field, int b)
-{
-  lua_pushvalue(L, idx);
-  lua_pushboolean(L, b);
-  lua_setfield(L, -2, field);
-  lua_pop(L, 1);
-}
 
 void set_version (lua_State *L, const struct serviceDeductions *sd)
 {
@@ -245,7 +224,7 @@ static int aux_mutex (lua_State *L)
       }
       lua_pushthread(L);
       lua_rawseti(L, lua_upvalueindex(1), lua_objlen(L, lua_upvalueindex(1))+1);
-      return nse_yield(L);
+      return nse_yield(L, 0, NULL);
     case DONE:
       lua_pushthread(L);
       if (!lua_equal(L, -1, lua_upvalueindex(2)))
@@ -341,7 +320,7 @@ static int aux_condvar (lua_State *L)
     case WAIT:
       lua_pushthread(L);
       lua_rawseti(L, lua_upvalueindex(1), lua_objlen(L, lua_upvalueindex(1))+1);
-      return nse_yield(L);
+      return nse_yield(L, 0, NULL);
     case SIGNAL:
       n = lua_objlen(L, lua_upvalueindex(1));
       break;
@@ -398,56 +377,6 @@ static int l_condvar (lua_State *L)
   return 1; // condition variable closure
 }
 
-Target *get_target (lua_State *L, int index)
-{
-  int top = lua_gettop(L);
-  Target *target;
-  luaL_checktype(L, index, LUA_TTABLE);
-  lua_getfield(L, index, "targetname");
-  lua_getfield(L, index, "ip");
-  if (!(lua_isstring(L, -2) || lua_isstring(L, -1)))
-    luaL_error(L, "host table does not have a 'ip' or 'targetname' field");
-  if (lua_isstring(L, -2)) /* targetname */
-  {
-    nse_gettarget(L, -2); /* use targetname */
-    if (lua_islightuserdata(L, -1))
-      goto done;
-    else
-      lua_pop(L, 1);
-  }
-  if (lua_isstring(L, -1)) /* ip */
-    nse_gettarget(L, -1); /* use ip */
-  if (!lua_islightuserdata(L, -1))
-    luaL_argerror(L, 1, "host is not being processed right now");
-done:
-  target = (Target *) lua_touserdata(L, -1);
-  lua_settop(L, top); /* reset stack */
-  return target;
-}
-
-Port *get_port (lua_State *L, Target *target, Port *port, int index)
-{
-  Port *p = NULL;
-  int portno, protocol;
-  luaL_checktype(L, index, LUA_TTABLE);
-  lua_getfield(L, index, "number");
-  if (!lua_isnumber(L, -1))
-    luaL_error(L, "port 'number' field must be a number");
-  lua_getfield(L, index, "protocol");
-  if (!lua_isstring(L, -1))
-    luaL_error(L, "port 'protocol' field must be a string");
-  portno = (int) lua_tointeger(L, -2);
-  protocol = strcmp(lua_tostring(L, -1), "tcp") == 0 ? IPPROTO_TCP :
-             strcmp(lua_tostring(L, -1), "udp") == 0 ? IPPROTO_UDP :
-             strcmp(lua_tostring(L, -1), "sctp") == 0 ? IPPROTO_SCTP :
-             luaL_error(L, "port 'protocol' field must be \"udp\", \"sctp\" or \"tcp\"");
-  while ((p = target->ports.nextPort(p, port, protocol, PORT_UNKNOWN)) != NULL)
-    if (p->portno == portno)
-      break;
-  lua_pop(L, 2);
-  return p;
-}
-
 /* Generates an array of port data for the given host and leaves it on
  * the top of the stack
  */
@@ -457,7 +386,8 @@ static int l_get_ports (lua_State *L)
       "open|filtered", "closed|filtered", NULL};
   static const int states[] = {PORT_OPEN, PORT_FILTERED, PORT_UNFILTERED,
       PORT_CLOSED, PORT_OPENFILTERED, PORT_CLOSEDFILTERED};
-  Port *p = NULL, port;
+  Port *p = NULL;
+  Port port; /* dummy Port for nextPort */
   Target *target = get_target(L, 1);
   int protocol = NSE_PROTOCOL[luaL_checkoption(L, 3, NULL, NSE_PROTOCOL_OP)];
   int state = states[luaL_checkoption(L, 4, NULL, state_op)];
@@ -487,7 +417,8 @@ static int l_get_ports (lua_State *L)
 static int l_get_port_state (lua_State *L)
 {
   Target *target;
-  Port port, *p;
+  Port *p;
+  Port port; /* dummy Port */
   target = get_target(L, 1);
   p = get_port(L, target, &port, 2);
   if (p == NULL)
@@ -522,7 +453,8 @@ static int l_set_port_state (lua_State *L)
   static const int opstate[] = {PORT_OPEN, PORT_CLOSED};
   static const char *op[] = {"open", "closed", NULL};
   Target *target;
-  Port port, *p;
+  Port *p;
+  Port port;
   target = get_target(L, 1);
   if ((p = get_port(L, target, &port, 2)) != NULL)
   {
@@ -561,7 +493,8 @@ static int l_set_port_version (lua_State *L)
     "incomplete"
   };
   Target *target;
-  Port port, *p;
+  Port *p;
+  Port port;
   enum service_tunnel_type tunnel = SERVICE_TUNNEL_NONE;
   enum serviceprobestate probestate =
       opversion[luaL_checkoption(L, 3, "hardmatched", ops)];
@@ -839,19 +772,11 @@ int luaopen_nmap (lua_State *L)
   lua_settop(L, 0); // clear stack
   luaL_register(L, "nmap", nmaplib);
 
-  lua_newtable(L);
-  lua_createtable(L, 0, 1);
-  lua_pushliteral(L, "v");
-  lua_setfield(L, -2, "__mode");
-  lua_setmetatable(L, -2); // Allow closures to be collected (see l_mutex)
+  weak_table(L, 0, 0, "v"); /* allow closures to be collected (see l_mutex) */
   lua_pushcclosure(L, l_mutex, 1); /* mutex function */
   lua_setfield(L, -2, "mutex");
 
-  lua_newtable(L);
-  lua_createtable(L, 0, 1);
-  lua_pushliteral(L, "v");
-  lua_setfield(L, -2, "__mode");
-  lua_setmetatable(L, -2); // Allow closures to be collected (see l_condvar)
+  weak_table(L, 0, 0, "v"); /* allow closures to be collected (see l_condvar) */
   lua_pushcclosure(L, l_condvar, 1); // condvar function
   lua_setfield(L, -2, "condvar");
 
@@ -860,6 +785,10 @@ int luaopen_nmap (lua_State *L)
 
   lua_pushcclosure(L, luaopen_nsock, 0);
   lua_pushliteral(L, "nsock");
+  lua_call(L, 1, 0);
+
+  lua_pushcclosure(L, luaopen_dnet, 0);
+  lua_pushliteral(L, "dnet");
   lua_call(L, 1, 0);
 
   lua_settop(L, 1); // just nmap lib on stack

commit 1b33f1da8f0d8b9f7243b8ea6d5c051ad08c7ded

Fix nse_dnet.cc compilation on Windows. This was broken in the merge in r20267.
git-svn-id: https://svn.nmap.org/nmap@20326 e0a8ed71-7df4-0310-8962-fdc924857419

nse_dnet.cc
@@ -182,7 +182,7 @@ static int ip_send (lua_State *L)
 {
   nse_dnet_udata *udata = (nse_dnet_udata *) luaL_checkudata(L, 1, DNET_METATABLE);
   const char *packet = luaL_checkstring(L, 2);
-
+  char dev[16];
   int ret;
 
   if (udata->sock == -1)
@@ -190,6 +190,8 @@ static int ip_send (lua_State *L)
 
   if (lua_objlen(L, 2) < sizeof(struct ip))
     return luaL_error(L, "ip packet too short");
+
+  *dev = '\0';
 
   if (o.sendpref & PACKET_SEND_ETH)
   {
@@ -208,6 +210,8 @@ static int ip_send (lua_State *L)
 
     if (!nmap_route_dst(&dstss, &route))
       goto usesock;
+
+    Strncpy(dev, route.ii.devname, sizeof(dev));
 
     if (route.ii.device_type != devt_ethernet)
       goto usesock;